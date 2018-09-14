3312570be04020f8dcbb3257a952a849675868d5
Nickolay Svetlov <ns@idaproject.com>
2018-09-14 13:31:35 +0300

    done func-component/Films

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,20 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function getStarsList(counter = 0, list = []) {
+  if (counter > 0) {
+    list.push(<Star/>);
+    return getStarsList(counter - 1, list);
+  }
+
+  return list;
 }
+
+function Stars({count}) {
+  const starList = getStarsList(count);
+
+  return <ul className="card-body-stars u-clearfix">{starList.map(star => <li>{star}</li>)}</ul>;
+}
+
+Stars.defaultProps = {
+  count: 5
+};
