ef7a28d8a45aeb332ac06532e2777b8718862b9d
Nickolay Svetlov <ns@idaproject.com>
2018-09-16 23:58:03 +0300

    add AuthForm

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const AuthForm = ({actionUrl, onAuth}) => {
+  const submitForm = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="ModalForm" onSubmit={submitForm} action={actionUrl} method="POST">
+      <Name/>
+      <Email/>
+      <Password/>
+      <button onClick={onAuth} type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right"/>
+      </button>
+    </form>
+  )
+};
