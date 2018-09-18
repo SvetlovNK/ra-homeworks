fee038cfaafb7b7c5a2eb82cc641c63cd0ab27ba
Nickolay Svetlov <ns@idaproject.com>
2018-09-18 21:45:02 +0300

    remove unusable inputRef

--- events/auth/js/Input.js
+++ events/auth/js/Input.js
@@ -1,8 +1,8 @@
-const Input = ({type, name, placeholder = '', required = true, inputRef, handleChange}) => {
+const Input = ({type, name, placeholder = '', required = true, handleChange}) => {
 
   return (
     <div className="Input">
-      <input required={required} type={type} name={name} placeholder={placeholder} inputRef={inputRef} onChange={handleChange}/>
+      <input required={required} type={type} name={name} placeholder={placeholder} onChange={handleChange}/>
       <label/>
     </div>
   )
--- events/auth/js/utils/validatePassword.js
+++ events/auth/js/utils/validatePassword.js
@@ -1,4 +1,9 @@
 const validatePassword = (string) => {
+  const TYPES = {
+    password: /[\\W]/gi,
+    email: /[^\w/@.-]/gi
+  };
+
   const pattern = /[\W]/gi;
 
   return string.replace(pattern, '');
