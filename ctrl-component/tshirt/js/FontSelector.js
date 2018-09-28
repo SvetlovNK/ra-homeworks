f913eea5f4936d3e2231b643f5b51c5b4f8ff619
Nickolay Svetlov <ns@idaproject.com>
2018-09-28 23:23:38 +0300

    prettify ctrl-component/tshirt

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,7 @@
 const FontSelector = ({fonts, selectedFont, onSelect}) => {
-    return (
-        <div className="font-picker">
-            Выберите шрифт
-        </div>
-    )
+  return (
+    <div className="font-picker">
+      Выберите шрифт
+    </div>
+  )
 };
