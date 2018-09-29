cb866a5b9aa84efd3bbb408f95e90650829d32bf
Nickolay Svetlov <ns@idaproject.com>
2018-09-29 23:12:05 +0300

    change TextRenderLine.js, FontSelecctor

--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,13 @@
 const TextRenderLine = ({value, onChange}) => {
+  const handleChange = (event) => {
+    const target = event.target;
+
+    onChange(target.value);
+  };
+
   return (
     <div className="type-text">
-      Наберите текст
+      <textarea name="text" id="font-text" onChange={handleChange} value={value} cols="30" rows="2" placeholder="Введите текст для футболки"></textarea>
     </div>
   );
 };
