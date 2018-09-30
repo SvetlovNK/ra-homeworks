d8d17e522f8d8bd33b20a53a0c757c472caf9881
Nickolay Svetlov <ns@idaproject.com>
2018-09-30 16:41:58 +0300

    change textarea

--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,13 +1,13 @@
 const TextRenderLine = ({value, onChange}) => {
   const handleChange = (event) => {
     const target = event.target;
-
     onChange(target.value);
   };
 
   return (
     <div className="type-text">
-      <textarea name="text" id="font-text" onChange={handleChange} value={value} cols="30" rows="2" placeholder="Введите текст для футболки"></textarea>
+      <textarea name="text" id="font-text" onChange={handleChange} value={value} cols="30" rows="2"
+                placeholder="Введите текст для футболки"/>
     </div>
   );
 };
