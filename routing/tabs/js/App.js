c07ffba554dc3719358f8de5ee6ae8646277d293
Nickolay Svetlov <ns@idaproject.com>
2018-10-03 22:57:40 +0300

    done routing/tabs

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,35 @@
+const App = () => {
+  const routes = [
+    {
+      id: 'general',
+      url: '/',
+      title: 'Рефераты',
+    },
+    {
+      id: 'creator',
+      url: '/creator',
+      title: 'Криэйтор',
+    },
+    {
+      id: 'fortune',
+      url: '/fortune',
+      title: 'Гадалка',
+    },
+  ];
 
+  return (
+    <Router>
+      <div>
+        <nav className="tabs__items">
+          {routes.map(route => <NavLink to={route.url} className="tabs__item" activeClassName="tabs__item-active" exact>{route.title}</NavLink>)}
+        </nav>
+
+        <div className="tabs__content">
+          <Route path="/" exact component={Essay}/>
+          <Route path="/creator" exact component={Creator}/>
+          <Route path="/fortune" exact component={Fortune}/>
+        </div>
+      </div>
+    </Router>
+  )
+};
