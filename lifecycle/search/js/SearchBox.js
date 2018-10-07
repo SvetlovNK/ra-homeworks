c3f03554f646e98a45a2855c63fda414c23498cb
Nickolay Svetlov <ns@idaproject.com>
2018-10-07 10:05:00 +0300

    done lifecycle/search

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -2,18 +2,47 @@ class SearchBox extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { fixed: false };
+    this.state = {fixed: false};
+    this.searchRef = undefined;
+    this.offsetTop = undefined;
+  }
+
+  handleScroll = (event) => {
+    const document = event.target.documentElement;
+    const scrollTop = document.scrollTop;
+    const searchOffsetTop = this.searchRef.offsetTop;
+
+    this.isFixed(scrollTop, searchOffsetTop);
+  };
+
+  isFixed = (scrolledTop, searchOffset) => {
+    const {fixed} = this.state;
+
+    if (fixed && scrolledTop - searchOffset <= this.offsetTop) {
+      this.setPosition(false);
+      return;
+    }
+
+    if (searchOffset - scrolledTop < 0) {
+      this.setPosition(true);
+    }
+  };
+
+  setPosition = (isFixed = true) => {
+    this.setState({fixed: isFixed});
+  };
+
+  componentDidMount() {
+    this.offsetTop = this.searchRef.offsetTop;
+
+    window.addEventListener('scroll', this.handleScroll);
   }
 
   render() {
-    return <SearchBoxView fixed={this.state.fixed} />
+    return <SearchBoxView getRef={el => this.searchRef = el} fixed={this.state.fixed}/>
   }
 
-  isFixed() {
-    return undefined;
-  }
-
-  setPosition() {
-    return undefined;
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
   }
 }
--- lifecycle/search/js/SearchBoxView.js
+++ lifecycle/search/js/SearchBoxView.js
@@ -1,8 +1,9 @@
-const SearchBoxView = ({ fixed }) => (
+const SearchBoxView = ({ getRef, fixed }) => (
   <section className="container">
     <div className="row">
       <div className="col-sm-12">
         <input
+          ref={getRef}
           className={`search-box ${fixed ? 'search-box_fixed' : null}`}
           placeholder="Поиск"
         >
