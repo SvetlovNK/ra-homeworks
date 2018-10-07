711938462e793c37b30dc8d4d9aef40ab1eb60f9
Nickolay Svetlov <ns@idaproject.com>
2018-10-07 13:46:16 +0300

    add ProgressBar functions:  drawProgress, drawCircle.

--- lifecycle/lib/js/ProgressBar.js
+++ lifecycle/lib/js/ProgressBar.js
@@ -1,7 +1,58 @@
 class ProgressBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.canvasRef = undefined;
+    this.progressLines = [
+      {
+        name: 'total',
+        radius: '45',
+        color: '#4ca89a'
+      },
+      {
+        name: 'progress',
+        radius: '38',
+        color: '#96d6f4'
+      }
+    ];
+  }
+
+  getCanvasRef = node => this.canvasRef = node;
+
+  drawProgress = () => {
+    const {total, completed} = this.props;
+    const canvas = this.canvasRef;
+    const ctx = canvas.getContext('2d');
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const [totalLine, progressLine] = this.progressLines;
+    const percent = completed / total * 100;
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    this.drawCircle(ctx, centerX, centerY, totalLine, 0, 2 * Math.PI);
+    this.drawCircle(ctx, centerX, centerY, progressLine, 0, percent / 100 * 2 * Math.PI);
+  };
+
+  drawCircle = (context, xPos, yPos, circleObject, startAngle, endAngle, anticlockwise = false) => {
+    context.beginPath();
+    context.strokeStyle = circleObject.color;
+    context.lineCap="round";
+    context.lineWidth = '7';
+    context.arc(xPos, yPos, circleObject.radius, startAngle, endAngle, anticlockwise);
+    context.stroke();
+  };
+
   render() {
     return (
-      <canvas id="progressCanvas" className="progress" />
+      <canvas ref={this.getCanvasRef} width="100px" height="100px" id="progressCanvas" className="progress"/>
     );
+  }
+
+  componentDidMount() {
+    this.drawProgress();
+  }
+
+  componentDidUpdate() {
+    this.drawProgress();
   }
 }
