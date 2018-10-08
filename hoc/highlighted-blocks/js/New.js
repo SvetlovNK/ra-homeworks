ad1d7d0ba83c19c44438e051e78ef6cc0d806b27
Nickolay Svetlov <ns@idaproject.com>
2018-10-08 22:20:38 +0300

    pretty hoc/highlighted-blocks

--- hoc/highlighted-blocks/js/New.js
+++ hoc/highlighted-blocks/js/New.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const New = props => {
-    return (
-        <div className="wrap-item wrap-item-new">
-        <span className="label">New!</span>
-        {props.children}
-        </div>
-    )
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  )
 };
