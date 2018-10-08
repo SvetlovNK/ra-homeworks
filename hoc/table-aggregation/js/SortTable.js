bca76aeb53c9226cd599340ada464a1bc63cc8e7
Nickolay Svetlov <ns@idaproject.com>
2018-10-08 22:50:01 +0300

    pretty hoc/table-aggregation

--- hoc/table-aggregation/js/SortTable.js
+++ hoc/table-aggregation/js/SortTable.js
@@ -2,23 +2,23 @@
 
 const SortTable = props => {
 
-    console.log('SortTable', props);
+  console.log('SortTable', props);
 
-    return (
-        <div>
-            <h2>Sort Table</h2>
-            <table>
-                <tr>
-                    <th>Date</th>
-                    <th>Amount</th>
-                </tr>
-                {props.list.map(item => (
-                    <tr>
-                        <td>{item.date}</td>
-                        <td>{item.amount}</td>
-                    </tr>
-                ))}
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Sort Table</h2>
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Amount</th>
+        </tr>
+        {props.list.map(item => (
+          <tr>
+            <td>{item.date}</td>
+            <td>{item.amount}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 };
