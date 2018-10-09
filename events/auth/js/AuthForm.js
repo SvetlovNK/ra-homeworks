f0504cc98ca550c3a0d99ef1e4d8236ab95d3ea1
Nickolay Svetlov <ns@idaproject.com>
2018-10-09 13:40:47 +0300

    refactor events/auth

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,31 +1,50 @@
 'use strict';
 
 const AuthForm = ({actionUrl, onAuth}) => {
-  const authData = {
-    name: undefined,
-    email: undefined,
-    password: undefined,
-  };
 
-  const onInputChange = (target) => {
-    const name = target.name;
+  const handleChange = (event) => {
+    const target = event.target;
+    const validatePattern = target.getAttribute('data-validate-pattern');
     const value = target.value;
+    const validatedValue = validatePattern ? validate(value, validatePattern) : value;
 
-    authData[name] = value;
+    target.value = validatedValue;
   };
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    if(!onAuth || !isFunction(onAuth)) return;
+  const submitForm = (event) => {
+    if (!onAuth || !isFunction(onAuth)) return;
 
-    onAuth(authData);
+    event.preventDefault();
+
+    const elements = event.target.elements;
+    const {name, email, password} = elements;
+
+    const data = {
+      name: name.value,
+      email: email.value,
+      password: password.value,
+    };
+
+    onAuth(data);
   };
 
   return (
     <form className="ModalForm" onSubmit={submitForm} action={actionUrl} method="POST">
-      <Name onInputChange={onInputChange}/>
-      <Email onInputChange={onInputChange}/>
-      <Password onInputChange={onInputChange}/>
+      <div className="Input">
+        <input type="text" name="name" onChange={handleChange} placeholder="Имя" required/>
+        <label/>
+      </div>
+
+      <div className="Input">
+        <input type="email" name="email" onChange={handleChange} data-validate-pattern="email" placeholder="Электронная почта" required/>
+        <label/>
+      </div>
+
+      <div className="Input">
+        <input type="password" name="password" placeholder="Пароль" data-validate-pattern="password"  required/>
+        <label/>
+      </div>
+
       <button type="submit">
         <span>Войти</span>
         <i className="fa fa-fw fa-chevron-right"/>
